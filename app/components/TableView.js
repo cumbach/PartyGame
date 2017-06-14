823ed76d565e7d720137a51f34b52b7939c7b360
Huan Ji <huan@transcriptic.com>
2017-06-14 15:31:37 -0700

    Refactor TableVisuals component to be reusable, add highlight player

--- app/components/TableView.js
+++ app/components/TableView.js
@@ -7,81 +7,43 @@ import {
 } from 'react-native';
 import { Actions } from 'react-native-router-flux';
 import { connect } from 'react-redux';
-import { Platform, ART } from 'react-native'
-const { Surface, Shape, Path, Group } = ART
 
-import { colors } from '../config/data';
+import TableVisuals from './TableVisuals';
 
 class TableView extends Component {
   constructor(props) {
      super(props);
-  }
 
-  createPath(cx, cy, r, startAngle, arcAngle) {
-    const p = new Path()
-    if (Platform.OS === 'ios') {
-      p.path.push(0, cx + r * Math.cos(startAngle), cy + r * Math.sin(startAngle))
-      p.path.push(4, cx, cy, r, startAngle, startAngle + arcAngle, 1)
-    } else {
-      // For Android we have to resort to drawing low-level Path primitives, as ART does not support
-      // arbitrary circle segments. It also does not support strokeDash.
-      // Furthermore, the ART implementation seems to be buggy/different than the iOS one.
-      // MoveTo is not needed on Android
-      p.path.push(4, cx, cy, r, startAngle, startAngle - arcAngle, 0)
-    }
-    return p
+     this.state = {
+       viewHeight: undefined,
+       viewWidth: undefined,
+       highlightIdx: undefined
+     }
   }
 
   render() {
-    let startValue = 0;
-    const playerCount = this.props.players.playerCount;
-    const viewHeight = 375;
-    const viewWidth = 667;
-    const radius = 120;
-
-    const centerX = Math.ceil(viewWidth / 2) - 20;
-    const centerY = Math.ceil(viewHeight / 2) - 20;
-
-    const width = 1;
-    const backgroundPath = this.createPath(radius, radius, radius - width / 2, 0, 360);
-    const lengthFromCenter = radius / 1.5;
-
     return (
       <View
         style={styles.container}
         onLayout={(event) => {
-          const { x, y, width, height } = event.nativeEvent.layout;
-          console.log(width);
+          const { width, height } = event.nativeEvent.layout;
+          this.setState({ viewWidth: width, viewHeight: height })
         }}
       >
-        <Surface
-          width={radius * 2}
-          height={radius * 2}>
-          <Group rotation={-90} originX={radius} originY={radius}>
-            <Shape d={backgroundPath} stroke={'black'} strokeWidth={width}/>
-          </Group>
-        </Surface>
         {
-          [...Array(playerCount)].map((_, idx) => {
-
-            const degree = 360 / playerCount * idx;
-            const radian = degree / (180 / Math.PI);
-            const x = Math.cos(radian) * lengthFromCenter;
-            const y = Math.sin(radian) * lengthFromCenter;
-
-            return (
-              <TouchableOpacity
-                key={`Player${idx}`}
-                onPress={() => undefined}
-                style={[{
-                  backgroundColor: colors[idx],
-                  left: centerX - y,
-                  top: centerY + x
-                }, styles.circle]}
-              >
-              </TouchableOpacity>
-            )
-          })
+          this.state.viewHeight && this.state.viewWidth ?
+            <TableVisuals
+              playerCount={this.props.players.playerCount}
+              viewHeight={this.state.viewHeight}
+              viewWidth={this.state.viewWidth}
+              onPlayerTouch={(playerIdx) => {
+                this.setState({
+                  selectedPlayer: playerIdx
+                });
+              }}
+              highlightIdx={this.state.selectedPlayer}
+            /> :
+            undefined
         }
         <TouchableOpacity
           key='ready'
@@ -100,19 +62,10 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     backgroundColor: 'white',
   },
-  circle: {
-    borderWidth:2,
-    borderColor:'rgba(0,0,0,0.4)',
-    alignItems:'center',
-    justifyContent:'center',
-    width:40,
-    height:40,
-    borderRadius:40,
-    marginBottom: 0, //Needs adjusting
-    position: 'absolute'
-  },
   ready: {
-    position: 'absolute', // Take out when possible
+    position: 'absolute',
+    bottom: 10,
+    left: -40,
     borderWidth:1,
     backgroundColor: 'lightgreen',
     overflow:'hidden', // doesn't work on Android??
@@ -124,24 +77,8 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     width: 130,
     left: -65 // Take out when positioning working correctly
+    margin: 10,
   }
 });
 
 export default connect(({ players }) => ({ players }))(TableView);
-
-// {[25, 25, 25, 25].map((item, idx) => {
-//   startAngle = startValue / 100 * 2  * Math.PI
-//   arcAngle = item / 100 * 2  * Math.PI
-//   startValue = startValue + item
-//   const path  = this.createPath(radius, radius, radius - width / 2, startAngle, arcAngle)
-//
-//   return (
-//     <Shape key={idx} d={path} stroke={'white'} strokeWidth={10} strokeCap='butt'>
-//       <TouchableOpacity
-//         key={`circle${idx}`}
-//         onPress={() => undefined}
-//         style={[{backgroundColor: 'black'}, styles.circle]}>
-//       </TouchableOpacity>
-//     </Shape>
-//   )
-// })}
---
+++ app/components/TableVisuals.js
@@ -0,0 +1,110 @@
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  View,
+  TouchableOpacity
+} from 'react-native';
+import { Platform, ART } from 'react-native'
+const { Surface, Shape, Path, Group } = ART
+
+import { colors } from '../config/data';
+
+class TableVisuals extends Component {
+  constructor(props) {
+     super(props);
+  }
+
+  createPath(cx, cy, r, startAngle, arcAngle) {
+    const p = new Path()
+    if (Platform.OS === 'ios') {
+      p.path.push(0, cx + r * Math.cos(startAngle), cy + r * Math.sin(startAngle))
+      p.path.push(4, cx, cy, r, startAngle, startAngle + arcAngle, 1)
+    } else {
+      // For Android we have to resort to drawing low-level Path primitives, as ART does not support
+      // arbitrary circle segments. It also does not support strokeDash.
+      // Furthermore, the ART implementation seems to be buggy/different than the iOS one.
+      // MoveTo is not needed on Android
+      p.path.push(4, cx, cy, r, startAngle, startAngle - arcAngle, 0)
+    }
+    return p
+  }
+
+  shouldHighlight(idx) {
+    return idx === this.props.highlightIdx;
+  }
+
+  render() {
+    let startValue = 0;
+    const playerCount = this.props.playerCount;
+    const radius = 120;
+
+    const centerX = radius - 20;
+    const centerY = Math.ceil(this.props.viewHeight / 2) - 20;
+
+    const width = 1;
+    const backgroundPath = this.createPath(radius, radius, radius - width / 2, 0, 360);
+    const lengthFromCenter = radius / 1.5;
+
+    return (
+      <View
+        style={styles.container}
+      >
+        <Surface
+          width={radius * 2}
+          height={radius * 2}>
+          <Group rotation={-90} originX={radius} originY={radius}>
+            <Shape d={backgroundPath} stroke={'black'} strokeWidth={width}/>
+          </Group>
+        </Surface>
+        {
+          [...Array(playerCount)].map((_, idx) => {
+
+            const degree = 360 / playerCount * idx;
+            const radian = degree / (180 / Math.PI);
+            const x = Math.sin(radian) * lengthFromCenter;
+            const y = Math.cos(radian) * lengthFromCenter;
+
+            return (
+              <TouchableOpacity
+                key={`Player${idx + 1}`}
+                onPress={() => this.props.onPlayerTouch(idx)}
+                style={[{
+                  backgroundColor: colors[idx],
+                  left: centerX - x,
+                  top: centerY + y,
+                  shadowColor: this.shouldHighlight(idx) ? '#000' : undefined,
+                  shadowOffset: this.shouldHighlight(idx) ? { width: 1, height: 3 } : undefined,
+                  shadowOpacity: this.shouldHighlight(idx) ? 0.8 : undefined,
+                  shadowRadius: this.shouldHighlight(idx) ? 3 : undefined
+                }, styles.circle]}
+              >
+              </TouchableOpacity>
+            )
+          })
+        }
+      </View>
+    );
+  }
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'white',
+  },
+  circle: {
+    borderWidth:2,
+    borderColor:'rgba(0,0,0,0.4)',
+    alignItems:'center',
+    justifyContent:'center',
+    width:40,
+    height:40,
+    borderRadius:40,
+    marginBottom: 0, //Needs adjusting
+    position: 'absolute'
+  }
+});
+
+export default TableVisuals;
